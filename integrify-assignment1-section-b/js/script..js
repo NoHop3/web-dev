405cdf74ed40283369c2758c545bc9e29b00a338
NoHop3 <u_sux@abv.bg>
2021-11-23 00:33:11 +0100

    Initial commit section b

---
+++ integrify-assignment1-section-b/js/script..js
@@ -0,0 +1,19 @@
+const clrAccent = "#16e0bd";
+const clrLight = "fff";
+const clrDark = "#303030";
+
+var taskToAdd = document.querySelector(".input__task");
+var addBtn = document.querySelector(".addBtn");
+var removeBtn = document.querySelector(".removeBtn");
+var removeAllBtn = document.querySelector(".removeAllBtn");
+
+
+// Add a task func
+function displayText() {
+    console.log(taskToAdd.value);
+}
+
+// Complete a task func
+function completeTask() {
+    
+}
